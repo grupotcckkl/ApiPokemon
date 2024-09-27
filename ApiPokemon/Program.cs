1ac41b77e326d775a77b732feec2ebd5a8926084
grupotcckkl <kaiokaykelaura.tcc@gmail.com>
2024-09-27 09:30:26 -0300

    configurado politica Cors

--- ApiPokemon/Program.cs
+++ ApiPokemon/Program.cs
@@ -14,7 +14,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowCrossOrigin", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowCrossOrigin");
+
 using (var scope = app.Services.CreateScope()) {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<PokemonContext>();
